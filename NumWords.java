2f961ca6f24d9dd484952ba8065b21ba4c837a36
Mor Cohen <mor.cohen02@post.runi.ac.il>
2024-11-07 15:44:06 +0200

    Mor Cohen

--- NumWords.java
+++ NumWords.java
@@ -3,8 +3,6 @@ public class NumWords {
 	public static void main(String args[]) {
 		// user enters a number (any digit number)
 		int n = Integer.parseInt(args[0]);
-		// n gets the last 3 numbers of the entered sum
-		n = Math.floorMod(n, 1000);
 		// system prints num of hundreds, tens, ones
 		System.out.println(n/100 + " hundreds, " + Math.floorMod(n, 100)/10 + " tens, and " + Math.floorMod(n, 10) + " ones.");
 	}
